5294e32e3167db59f1ef6f0b754b8474121b2b0c
rey <alasasrey5@gmail.com>
2025-05-01 21:36:50 +0800

    frontend website

---
+++ src/server-php-laravel/server.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const fetchReports = async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/api/create-report', {
+    });
+
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error fetching reports:', error);
+  }
+};
